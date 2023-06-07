3fc2ea89b96aa757bf37842c7bf57dc2efbb78d7
Sachin Gawas <sachin.gawas@yahoo.com>
2023-06-07 14:54:53 +0100

    First commit of ML

---
+++ score.js
@@ -0,0 +1,10 @@
+const outputs = [];
+
+function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
+outputs.push([dropPosition, bounciness, size, bucketLabel]);
+console.log(outputs);
+}
+
+function runAnalysis() {
+
+}
